a3e109310a1285f25cc5d284b4cfb021e297f641
astonelv <158239756+astonelv@users.noreply.github.com>
2024-03-20 17:58:32 -0700

    Update move.js

--- move.js
+++ move.js
@@ -10,3 +10,76 @@ function move(element) {
         to: moveToCoordinates
     }
 }
+
+function moveWithArrowKeys(left, bottom, handleDirectionChange){
+    let direction = null;
+    let x = left;
+    let y = bottom;
+
+function moveCharacter(){
+    if(direction === 'west'){
+        x = x - 1
+    }
+    if(direction === 'east'){
+        x = x + 1
+    }
+    if(direction === 'north'){
+        y = y + 1
+    }
+    if(direction === 'south'){
+        y = y - 1
+    }
+    character.style.left = x + 'px'
+    character.style.bottom = y + 'px'
+}
+
+setInterval(moveCharacter, 1)
+
+document.addEventListener('keydown', function(e){
+    if(e.repeat) return;
+
+    if(e.key === 'ArrowLeft'){
+        direction = 'west'
+    }
+    if(e.key === 'ArrowUp'){
+        direction = 'north'
+    }
+    if(e.key === 'ArrowRight'){
+        direction = 'east'
+    }
+    if(e.key === 'ArrowDown'){
+        direction = 'south'
+    }
+    handleDirectionChange(direction)
+})
+
+document.addEventListener('keyup', function(e){
+    direction = null;
+    handleDirectionChange(direction)
+})
+
+}
+
+const character = newImage('assets/green-character/static.gif')
+
+function handleDirectionChange(direction){
+    if(direction === null){
+        character.src = 'assets/green-character/static.gif'
+    }
+    if(direction === 'west'){
+        character.src = 'assets/green-character/west.gif'
+    }
+    if(direction === 'north'){
+        character.src = 'assets/green-character/north.gif'
+    }
+    if(direction === 'east'){
+        character.src = 'assets/green-character/east.gif'
+    }
+    if(direction === 'south'){
+        character.src = 'assets/green-character/south.gif'
+    }
+}
+
+move(character).moveWithArrowKeys(100, 250, handleDirectionChange)
+
+
